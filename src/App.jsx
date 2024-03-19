a99c96ff815a085a6ab575c7fb3c3562c68e7190
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-03-19 22:50:29 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Script from "./Components/Header/Script";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const current_theme = localStorage.getItem("current_theme");
+  const [theme, setTheme] = useState(current_theme? current_theme : "light");
+
+  useEffect(() =>{
+    localStorage.setItem("current_theme", theme);
+  }, [theme])
 
   return (
-    <>
-      <div>
-        <h1> H1 Sanjeet kumar mddddd</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={`container ${theme}`}>
+      <Script theme={theme} setTheme={setTheme} />
+    </div>
+  );
 }
 
-export default App
+export default App;
